485ceba7f8c49d6a0375bd852c75b54359fb97eb
DESKTOP-V88JK8B\hi <vinhthai2905@aol.com>
2025-01-10 14:07:02 +0700

    completed day 3

---
+++ Exercises/Day_03/com_operators.py
@@ -0,0 +1,31 @@
+from ctypes import pythonapi
+
+x = 2
+
+sunny = True
+
+if not sunny:
+    print('It is cloudy')
+else:
+    print('It is sunny')
+
+print(not sunny)
+
+a = -3
+
+if not a > 0:
+    print('The number is not positive')
+else:
+    print('The number is positive')
+
+
+name = 'Bob'
+
+if not name == 'Bob':
+    print('Not Bob')
+else:
+    print('Bob')
+
+name_1 = 'python'
+name_2 = 'dragon'
+
