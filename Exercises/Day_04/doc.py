707fdefc6a65ed46fce9c67fec5024d383d368d0
DESKTOP-V88JK8B\hi <vinhthai2905@aol.com>
2025-01-10 18:33:38 +0700

    squash into 1 for feature 4

---
+++ Exercises/Day_04/doc.py
@@ -0,0 +1,16 @@
+multiline_string = '''I am a teacher and enjoy teaching.
+I didn't find anything as rewarding as empowering people.
+That is why I created 30 days of python.'''
+print(multiline_string)
+
+# To write a backslash
+
+print('This is a backslash  symbol (\\)')
+
+# Strings only
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+language = 'Python'
+formated_string = 'I am %s %s. I teach %s' %(first_name, last_name, language)
+print(formated_string)
+
