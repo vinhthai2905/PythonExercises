fe47cf61b960ad8ce23c2d1d681497f86b7ce95d
DESKTOP-V88JK8B\hi <vinhthai2905@aol.com>
2025-01-10 18:33:38 +0700

    done ex1 and ex2

---
+++ Exercises/Day_04/exercises.py
@@ -0,0 +1,48 @@
+# Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+thirty_string = 'Thirty'
+days_string = 'Days'
+of_string = 'of'
+python_string = 'Python'
+strings = ['Thirty', 'Days', 'Of', 'Python']
+combined_strings = ' '.join(strings)
+print(f'Fully combined string: {thirty_string} {days_string} {of_string} {python_string}')
+print(f'Fully combined string: {combined_strings}')
+
+# Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+coding_strings = ['Coding', 'For', 'All']
+combined_strings = ' '.join(coding_strings)
+print(f'Fully combined string: {combined_strings}')
+
+# Declare a variable named company and assign it to an initial value "Coding For All".
+# Print the variable company using print().
+# Print the length of the company string using len() method and print().
+# Change all the characters to uppercase letters using upper() method.
+# Change all the characters to lowercase letters using lower() method.
+# Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+# Cut(slice) out the first word of Coding For All string.
+# Check if Coding For All string contains a word Coding using the method index, find or other methods.
+# Replace the word coding in the string 'Coding For All' to Python.
+# Change Python for Everyone to Python for All using the replace method or other methods.
+# Split the string 'Coding For All' using space as the separator (split()) .
+# "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+# What is the character at index 0 in the string Coding For All.
+# What is the last index of the string Coding For All.
+# What character is at index 10 in "Coding For All" string.
+# Create an acronym or an abbreviation for the name 'Python For Everyone'.
+# Create an acronym or an abbreviation for the name 'Coding For All'.
+# Use index to determine the position of the first occurrence of C in Coding For All.
+# Use index to determine the position of the first occurrence of F in Coding For All.
+# Use rfind to determine the position of the last occurrence of l in Coding For All People.
+# Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+# Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+# Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+# Find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+# Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+# Does ''Coding For All' start with a substring Coding?
+# Does 'Coding For All' end with a substring coding?
+# '   Coding For All      '  , remove the left and right trailing spaces in the given string.
+# Which one of the following variables return True when we use the method isidentifier():
+# 30DaysOfPython
+# thirty_days_of_python
+# The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
+# Use the new line escape sequence to separate the following sentences.
