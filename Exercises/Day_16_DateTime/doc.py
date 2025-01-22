53a09d6b791b7a1680f9c16c9e5fe888a68d4749
DESKTOP-V88JK8B\hi <vinhthai2905@aol.com>
2025-01-22 20:21:46 +0700

    practising datetime

--- Exercises/Day_16_DateTime/doc.py
+++ Exercises/Day_16_DateTime/doc.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 now = datetime.now()
 print(now)                      # 2021-07-08 07:34:46.549883
 day = now.day                   # 8
@@ -12,3 +12,19 @@ print(day, month, year, hour, minute)
 print('timestamp', timestamp)
 print(f'{day}/{month}/{year}, {hour}:{minute}')  # 8/7/2021, 7:38
 
+from datetime import datetime
+# current date and time
+now = datetime.now()
+t = now.strftime("%H:%M:%S")
+print("time:", t)
+time_one = now.strftime("%m/%d/%Y, %H:%M:%S")
+# mm/dd/YY H:M:S format
+print("time one:", time_one)
+time_two = now.strftime("%d/%m/%Y, %H:%M:%S, %a")
+# dd/mm/YY H:M:S format
+print("time two:", time_two)
+
+a = datetime(2020, 1, 1)
+print(a.today())
+d = date(2020, 1, 1)
+print(d.today())
