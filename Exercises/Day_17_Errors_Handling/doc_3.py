ce4c7b1828e26441f2684202be67760dca6eaf10
DESKTOP-V88JK8B\hi <vinhthai2905@aol.com>
2025-01-24 14:50:33 +0700

    done day 2 of day 14

--- Exercises/Day_17_Errors_Handling/doc_3.py
+++ Exercises/Day_17_Errors_Handling/doc_3.py
@@ -14,8 +14,18 @@ def sum_of_five_nums(*args):
 lst = [1, 2, 3, 4, 5, 'Hello']
 print(sum_of_five_nums(*lst))
 
-def unpacking_person_info(name, country, city, age):
-    return f'{name} lives in {country}, {city}. He is {age} year old.'
+def unpacking_person_info(**kwargs):
+    for key in kwargs:
+        print(key)
+
 dct = {'name':'Thai', 'country':'Finland', 'city':'Helsinki', 'age':250}
 
 print(unpacking_person_info(**dct)) # Thai lives in Finland, Helsinki. He is 250 years old.
+
+countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+fin, sw, nor, *rest = countries
+print(fin, sw, nor, rest)   # Finland Sweden Norway ['Denmark', 'Iceland']
+numbers = [1, 2, 3, 4, 5, 6, 7]
+one, *middle, last = numbers
+print(one, middle, last)      #  1 [2, 3, 4, 5, 6] 7
+
