b77d44fc60f0af477ca314e494767a4c8e8f8321
DESKTOP-V88JK8B\hi <vinhthai2905@aol.com>
2025-01-28 20:25:46 +0700

    testing special character

--- Exercises/Day_18_Regular_Expressions/test.py
+++ Exercises/Day_18_Regular_Expressions/test.py
@@ -2,12 +2,22 @@ import re
 
 test_pattern = r'^a{3,5}aa'
 
-string_test = 'aaaaa'
+string_test = 'aaaaaa'
+
+test_special_pattern = r'a\|b'
+
+string_test_2 = r'ayes'
 
 if re.search(test_pattern, string_test):
     print('yes')
 else:
     print('no')
+
+if re.match(test_special_pattern, string_test_2):
+    print('yes')
+else:
+    print('no')
+
 
 
 
