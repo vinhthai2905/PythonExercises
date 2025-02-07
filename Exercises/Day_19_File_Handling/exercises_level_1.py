71d225820c7307ac4b8b28df8ebbbdcbf8cd7855
DESKTOP-V88JK8B\hi <vinhthai2905@aol.com>
2025-02-07 22:21:08 +0700

    done ex1

--- Exercises/Day_19_File_Handling/exercises_level_1.py
+++ Exercises/Day_19_File_Handling/exercises_level_1.py
@@ -1,3 +1,5 @@
+import json
+from typing import Any
 # Write a function which count number of lines and number of words in a text.
 # All the files are in the data the folder:
 # a) Read obama_speech.txt file and count number of lines and words
@@ -16,9 +18,8 @@ with open('./ex_1/michelle_obama_speech.txt', mode='r+', encoding='utf-8') as mi
     line_count = 0
     word_count = 0
     list_speech = michelle_file.readlines()
-
     for line in list_speech:
-        if len(line) < 0:
+        if len(line) - line.count(' ') == 1:
             continue
         else:
             line_count += 1
@@ -28,3 +29,31 @@ with open('./ex_1/michelle_obama_speech.txt', mode='r+', encoding='utf-8') as mi
 # c) Read donald_speech.txt file and count number of lines and words d)
 # Read melina_trump_speech.txt file and count number of lines and words
 
+# Read the countries_data.json data file in data directory, create a function that finds the ten most spoken languages
+
+def most_spoken_languages():
+    with open('./ex_1/countries_data.json', mode='r+', encoding='utf-8') as countries_file:
+        ten_countries = dict()
+        existed_languages = list()
+        list_languages: list[dict[str, Any]] = []
+        countries_datas: list[dict[str, Any]] = json.load(countries_file)
+
+        for dict_country in countries_datas:
+            for language in dict_country.get('languages'):
+                if language in existed_languages:
+                    for existed_lang in list_languages:
+                        if language == existed_lang.get('language_name'):
+                            existed_lang['used_count'] += 1
+                            continue
+                else:
+                    existed_languages.append(language)
+                    list_languages.append({
+                        'language_name': language,
+                        'used_count': 1
+                    })
+
+        return list_languages
+
+list_languages = most_spoken_languages()
+
+print(list_languages)
