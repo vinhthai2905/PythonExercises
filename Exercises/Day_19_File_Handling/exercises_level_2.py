12a3e4c41c09840b6b6336a9aa8059cec72795dc
vinhthai2905-admin <vinhthai2905@aol.com>
2025-02-11 18:48:06 +0700

    done day 19

--- Exercises/Day_19_File_Handling/exercises_level_2.py
+++ Exercises/Day_19_File_Handling/exercises_level_2.py
@@ -1,26 +1,19 @@
-import json
-from collections import Counter
+import re
 
+try:
+    with open('./ex_2/email_exchange.txt', encoding='utf=8') as email_file:
+        email_pattern = r'\w+@\w+(\.\w+)+$'
+        list_emails = []
+        lines = email_file.readlines()
+        for line in lines:
+            if len(line) == 1:
+                continue
+            else:
+                if re.search(email_pattern, line):
+                    email = re.search(email_pattern, line)
+                    list_emails.append(email.group())
+except Exception as e:
+    print(e.__repr__())
 
-def most_spoken_languages():
-    with open('./ex_1/countries_data.json', mode='r+', encoding='utf-8') as countries_file:
-        countries_datas = json.load(countries_file)
-
-        language_counter = Counter()
-
-        for country in countries_datas:
-            languages = country.get('languages', [])
-            language_counter.update(languages)
-
-        most_common_languages = language_counter.most_common(10)
-
-        print(language_counter)
-        for language, count in most_common_languages:
-            print(f'{language}: {count}')
-
-
-most_spoken_languages()
-
-test = Counter('test')
-
-print(test)
+set_emails = set(list_emails)
+print(set_emails)
